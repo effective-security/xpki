3c9b6939f710bf7060a9077258cf2c40bb841825
Denis Issoupov <denis@ekspand.com>
2022-04-21 15:37:24 +0100

    xpki-tool created

---
+++ cmd/xpki-tool/cli/csr.go
@@ -0,0 +1,37 @@
+package cli
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+
+	"github.com/effective-security/xpki/x/print"
+	"github.com/pkg/errors"
+)
+
+// CsrInfoCmd specifies flags for Info command
+type CsrInfoCmd struct {
+	Csr string `kong:"arg" required:"" help:"CSR file name"`
+}
+
+// Run the command
+func (a *CsrInfoCmd) Run(ctx *Cli) error {
+	// Load CSR
+	csrb, err := ctx.ReadFile(a.Csr)
+	if err != nil {
+		return errors.WithMessage(err, "unable to load CSR file")
+	}
+
+	block, _ := pem.Decode(csrb)
+	if block == nil || "CERTIFICATE REQUEST" != block.Type {
+		return errors.New("invalid CSR file")
+	}
+
+	csrv, err := x509.ParseCertificateRequest(block.Bytes)
+	if err != nil {
+		return errors.WithMessage(err, "unable to prase CSR")
+	}
+
+	print.CertificateRequest(ctx.Writer(), csrv)
+
+	return nil
+}
---
+++ cmd/xpki-tool/cli/suite_test.go
@@ -0,0 +1,123 @@
+package cli
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/alecthomas/kong"
+	"github.com/effective-security/xpki/x/ctl"
+	"github.com/stretchr/testify/suite"
+)
+
+type testSuite struct {
+	suite.Suite
+	tmpdir string
+	ctl    *Cli
+	// Out is the outpub buffer
+	Out bytes.Buffer
+
+	appFlags []string
+}
+
+func (s *testSuite) SetupSuite() {
+	s.tmpdir = filepath.Join(os.TempDir(), "/tests/xpki", "xpki-tool")
+	err := os.MkdirAll(s.tmpdir, 0777)
+	s.Require().NoError(err)
+
+	s.ctl = &Cli{}
+
+	s.ctl.WithErrWriter(&s.Out).
+		WithWriter(&s.Out)
+
+	parser, err := kong.New(s.ctl,
+		kong.Name("xpki-tool"),
+		kong.Description("CLI tool"),
+		kong.Writers(&s.Out, &s.Out),
+		ctl.BoolPtrMapper,
+		//kong.Exit(exit),
+		kong.ConfigureHelp(kong.HelpOptions{
+			Compact: true,
+		}),
+		kong.Vars{})
+	if err != nil {
+		s.FailNow("unexpected error constructing Kong: %+v", err)
+	}
+
+	flags := s.appFlags
+	_, err = parser.Parse(flags)
+	if err != nil {
+		s.FailNow("unexpected error parsing: %+v", err)
+	}
+}
+
+func (s *testSuite) TearDownSuite() {
+	os.RemoveAll(s.tmpdir)
+}
+
+// HasText is a helper method to assert that the out stream contains the supplied
+// text somewhere
+func (s *testSuite) HasText(texts ...string) {
+	outStr := s.Out.String()
+	for _, t := range texts {
+		s.Contains(outStr, t)
+	}
+}
+
+// HasNoText is a helper method to assert that the out stream does contains the supplied
+// text somewhere
+func (s *testSuite) HasNoText(texts ...string) {
+	outStr := s.Out.String()
+	for _, t := range texts {
+		s.Contains(outStr, t)
+	}
+}
+
+// HasTextInFile is a helper method to assert that file contains the supplied text
+func (s *testSuite) HasTextInFile(file string, texts ...string) {
+	f, err := ioutil.ReadFile(file)
+	s.Require().NoError(err, "unable to read: %s", file)
+	outStr := string(f)
+	for _, t := range texts {
+		s.Contains(outStr, t, "expecting to find text %q in file %q", t, file)
+	}
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(testSuite))
+}
+
+func (s *testSuite) TestCrsInfo() {
+	cmd := CsrInfoCmd{
+		Csr: "../../../x/print/testdata/trusty_dev_peer.csr",
+	}
+	err := cmd.Run(s.ctl)
+	s.Require().NoError(err)
+	s.HasText()
+}
+
+func (s *testSuite) TestOcspInfo() {
+	cmd := OcspInfoCmd{
+		Ocsp: "testdata/ocsp1.res",
+	}
+	err := cmd.Run(s.ctl)
+	s.NoError(err)
+}
+
+func (s *testSuite) TestCertInfo() {
+	cmd := CertInfoCmd{
+		In: "../../../x/print/testdata/trusty_peer_wfe.pem",
+	}
+	err := cmd.Run(s.ctl)
+	s.NoError(err)
+}
+
+func (s *testSuite) TestCertValidate() {
+	cmd := CertValidateCmd{
+		Cert: "../../../x/print/testdata/trusty_peer_wfe.pem",
+	}
+	err := cmd.Run(s.ctl)
+	s.Error(err)
+}
