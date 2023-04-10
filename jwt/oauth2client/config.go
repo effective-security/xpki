e9cf73ea060161f0c5a681a7f7a357c320b18174
Denis Issoupov <denis@ekspand.com>
2023-04-10 12:50:56 +0100

    oauth: added IDPParam

--- jwt/oauth2client/config.go
+++ jwt/oauth2client/config.go
@@ -28,6 +28,8 @@ type ClientConfig struct {
 	Scopes []string `json:"scopes" yaml:"scopes"`
 	// ResponseType specifies the response type, default is "code"
 	ResponseType string `json:"response_type" yaml:"response_type"`
+	// JwksURL specifies JWKS URL
+	JwksURL string `json:"jwks_url" yaml:"jwks_url"`
 	// AuthURL specifies auth URL
 	AuthURL string `json:"auth_url" yaml:"auth_url"`
 	// TokenURL specifies token URL
@@ -44,6 +46,18 @@ type ClientConfig struct {
 	Audience string `json:"audience" yaml:"audience"`
 	// Issuer of JWT token
 	Issuer string `json:"issuer" yaml:"issuer"`
+	// IDPParam specifies the IDP parameters
+	IDPParam *IDPParam `json:"idp_param" yaml:"idp_param"`
+	// Domains specifies the list of domains to filter by
+	Domains []string `json:"domains" yaml:"domains"`
+}
+
+// IDPParam is a struct for IDP parameter
+type IDPParam struct {
+	// Name specifies the name of the IDP parameter: idpuser.email|identity_provider
+	Name string `json:"name" yaml:"name"`
+	// Value specifies the value the IDP parameter: email|domain|{value}
+	Value string `json:"value" yaml:"value"`
 }
 
 // LoadConfig returns configuration loaded from a file
